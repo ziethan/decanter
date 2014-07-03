a5b517e35c9aaf8cce5dbb33ec58aa308e03b9e2
Zach McGrenere <zach@mcgrenere.com>
2014-07-03 16:45:08 -0400

    Removed old files

--- app/uninstall/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, print_function
-
-import requests
-
-def Uninstall(package, version='latest', **kwargs):
-    print('Uninstalling {0} {1}.'.format(package, version))
